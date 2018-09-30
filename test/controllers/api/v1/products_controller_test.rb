d216e16a9a8fd66f91850102737fc003fe442498
Vitaly Stanchits <vitaly.stanchits@uni-potsdam.de>
2018-09-30 23:38:43 +0200

    yarn fix

--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -4,4 +4,5 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  # tests if the application starts at all
+  test "should get index if user offline" do
+    get root_path
+    assert_response :success
+  end
+
+  # tests if application starts if user is signed in
+  test "should get index if user online" do
+    @user = users(:sample_user_1)
+    sign_in @user
+
+    get root_path
+    assert_response :success
+  end
 end
